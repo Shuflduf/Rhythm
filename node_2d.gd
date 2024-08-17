f577831060e0710a2fa713c92c2309bd00ee96f6
Shuflduf <shuflduf@gmail.com>
2024-08-17 15:30:40 -0600

    audio loading

--- node_2d.gd
+++ node_2d.gd
@@ -1,17 +1,26 @@
 extends Node2D
 
+@export_global_file var track: String
+
+
 var inputs = ["one", "two", "three", "four"]
 @onready var keys: Node2D = $Keys
 @onready var notes: Node2D = $Notes
 
+func _ready() -> void:
+	$AudioStreamPlayer.stream = load(track)
+	$AudioStreamPlayer.play()
+
 func _process(delta: float) -> void:
-	notes.position.y += delta * 100
+	notes.position.y += delta * 300
 	for i in inputs.size():
 		var input = inputs[i]
+		var key = keys.get_child(i)
 
 		if Input.is_action_just_pressed(input):
-			keys.get_child(i).modulate = Color.BLACK
-			keys.get_child(i).get_overlapping_areas()[0].queue_free()
+			key.modulate = Color.BLACK
+			if key.has_overlapping_areas():
+				key.get_overlapping_areas()[0].queue_free()
 
 		if Input.is_action_just_released(input):
-			keys.get_child(i).modulate = Color.WHITE
+			key.modulate = Color.WHITE
