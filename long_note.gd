a9083258ded912083adb1c52b05284408f51aac2
Shuflduf <shuflduf@gmail.com>
2024-08-17 17:01:46 -0600

    basic long notes

--- long_note.gd
+++ long_note.gd
@@ -5,5 +5,6 @@ var shrinking = false
 
 func _process(delta: float) -> void:
 	if shrinking:
-		scale.y -= delta * (1.0 / get_parent().seconds_per_beat) * 20
-		position.y -= delta * (1.0 / get_parent().seconds_per_beat) * 20
+		scale.y -= delta * (1.0 / get_parent().seconds_per_beat) * 12
+		#position.y -= delta * (1.0 / get_parent().seconds_per_beat) * 50
+		scale.y = clamp(scale.y, 0, INF)
