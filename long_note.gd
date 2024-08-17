277456c65f0f97984d0b25161a20e27d04d7e881
Shuflduf <shuflduf@gmail.com>
2024-08-17 17:37:12 -0600

    mapping to 30

--- long_note.gd
+++ long_note.gd
@@ -6,5 +6,5 @@ var shrinking = false
 func _process(delta: float) -> void:
 	if shrinking:
 		scale.y -= delta * (1.0 / get_parent().seconds_per_beat) * 12
-		#position.y -= delta * (1.0 / get_parent().seconds_per_beat) * 50
 		scale.y = clamp(scale.y, 0, INF)
+		#if Input.is_action_just_released()
